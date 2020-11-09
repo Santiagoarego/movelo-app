3800c3f754074d66bcbf40290a0caa51e569ec95
davidHG12 <davidhiga@unisabana.edu.co>
2020-11-08 23:58:16 -0500

    Alerta pequeña

--- src/components/Panico.js
+++ src/components/Panico.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { StyleSheet, Text, View, Alert, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Alert, TouchableOpacity, Button} from "react-native";
+import AwesomeAlert from 'react-native-awesome-alerts';
 import BotonCircular from "./BotonCircular";
 
-export default function Panico() {
-  return (
-    <View style={styles.view}>
-      <Text></Text>
-      <Text style={{ color: "#fff", fontWeight: "bold", textAlign: "center" }}>
-        ¡Cuidado! Por favor, ten en cuenta que debes usar esta función de manera
-        honesta y seria.
-      </Text>
-      <Text></Text>
-      <BotonCircular onPress={() => console.log("Hola")} circleDiameter={300}>
+export default class Panico extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { showAlert: false };
+  };
+ 
+  showAlert = () => {
+    this.setState({
+      showAlert: true
+    });
+  };
+ 
+  hideAlert = () => {
+    this.setState({
+      showAlert: false
+    });
+  };
+ 
+  render() {
+    const {showAlert} = this.state;
+ 
+    return (
+      <View style={styles.view}>
+        <BotonCircular onPress={() => {
+          this.showAlert();
+        }}circleDiameter={300}>
+        </BotonCircular>
         <Text
           style={{
             color: "white",
@@ -22,15 +41,53 @@ export default function Panico() {
         >
           Alerta!
         </Text>
-      </BotonCircular>
-    </View>
-  );
-}
-
+ 
+        <AwesomeAlert
+          show={showAlert}
+          showProgress={false}
+          title="Alerta!"
+          message="¡Cuidado! Por favor, ten en cuenta que debes usar esta función de manera
+          honesta y seria."
+          closeOnTouchOutside={true}
+          closeOnHardwareBackPress={false}
+          showCancelButton={true}
+          showConfirmButton={true}
+          cancelText="Cancelar"
+          confirmText="Confirmar"
+          confirmButtonColor="#DD6B55"
+          onCancelPressed={() => {
+            this.hideAlert();
+          }}
+          onConfirmPressed={() => {
+            this.hideAlert();
+          }}
+        />
+      </View>
+    );
+  };
+};
+ 
 const styles = StyleSheet.create({
   view: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  button: {
+    margin: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 7,
+    borderRadius: 5,
+    backgroundColor: "#AEDEF4",
+  },
+  text: {
+    color: '#fff',
+    fontSize: 15
+  }
 });
